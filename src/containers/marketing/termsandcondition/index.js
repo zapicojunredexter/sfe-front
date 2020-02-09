648ff7448a33bf32457955e4dc8bad32c57e0b56
jlizl <jlizl@gmail.com>
2020-02-10 01:10:01 +0800

    Add content to terms and conditions
    Signed-off-by: jlizl <jlizl@gmail.com>

--- src/containers/marketing/termsandcondition/index.js
+++ src/containers/marketing/termsandcondition/index.js
@@ -6,7 +6,54 @@ class Container extends React.PureComponent<> {
 
     render(){
         return (
-            <h1>Hello World</h1>
+            <div class="container" style={{marginTop: '4em'}}>
+                <h1 class="text-center" style={{marginBottom: '.5em'}}> TERMS AND CONDITION FOR THE STREET FOOD EXPRESS</h1>
+                <p>These Terms and Conditions shall apply to all orders of Streetfood Express products of a customer through this street food Delivery Website.
+                     By placing an order for Products through this Site, you understand, agree with, and accept: </p>
+                <ul style={{listStyleType: "upper-roman"}}>
+                    <li> These Terms and Conditions and any corresponding changes thereto which we may implement from time to time; </li>
+                    <li> Our Privacy Policy in connection with how we process your personal information collected from this Site. </li>
+                    <li> Our Cash on Delivery  payment, the customer is obligue to pay the full payment upon delivery of the Ordered Product, if failure to meet the agreement will face the consequences</li>
+                </ul>
+                <b>1. Delivery Area</b>
+                <p style={{textIndent: '50px', marginBottom: '2em'}}>Streetfood Express offers delivery services only within Cebu only. If your location or delivery area is not within any of delivery areas mentioned above, you will be prompted upon placing your order online.
+                Each Streetfood Express delivery store has a dedicated delivery area which cannot be changed. Thus, any forced request for re-routing of an order to another store is strictly prohibited.</p>
+                
+                <b>2. Account Creation</b>
+                <p style={{textIndent: '50px', marginBottom: '2em'}}>
+                If you decide to place an order through this Site, you will be asked to register by creating an account which requires your personal information such as name, complete delivery address, telephone or mobile number, email address and a password. You must keep this password confidential and must not disclose it to anyone. If you believe someone else knows your password, please change your password immediately. We reserve the right to suspend your registration if you violate any of these Terms and Conditions.
+                If you wish to order as a guest, the site will require for your name, telephone or mobile number and complete delivery address.</p>
+                
+                <b>3. Submission of Order</b>
+                <p style={{textIndent: '50px', marginBottom: '2em'}}>Upon submission of your online order, you are authorizing the Vendor to process your online order and prepare the Products you have specified on your order form. However, we reserve the right to refuse the processing of your order without any notification should we find any incorrect or incomplete 
+                Also, your failure to respond to SMS and calls from the Store, Delivery Rider may result to late or non-delivery, which should not be taken against us. Additional verification calls or confirmation calls may be required to proceed with your order</p>
+               
+                <b>4. Order Acceptance and Confirmation</b>
+                <p style={{textIndent: '50px', marginBottom: '2em'}}>The acceptance of orders for the Products made through the Application shall be at the entire discretion of Streetfood Express. 
+                Our acceptance of an order occurs after you make an Order and During Payment their will be an Agreement message confirming that your order has been placed (“Order Confirmation”). The Order Confirmation is a record of our agreement to deliver the Products you ordered and your obligation to pay the price of the Products in full.</p>
+               
+                <b>5. Cancellation of Orders</b>
+                <p style={{textIndent: '50px', marginBottom: '2em'}}>Orders may be cancelled but the customer must pay the 30% of their full ordered products payment. We reserve the right to refuse any cancellation in case the order has already been dispatched for delivery prior to receipt of your request</p>
+                
+                <b>6. Prices and Payment</b>
+                <p style={{textIndent: '50px', marginBottom: '2em'}}>Unless otherwise stated in writing, the Product prices quoted on this Site are inclusive of value-added taxes, any other applicable taxes and delivery charge.
+                To check the price of the Products you want to order, please click the Checkout button to see the breakdown and total amount due.</p>
+                
+                <b>Delivery Conditions</b>
+                <p style={{textIndent: '50px', marginBottom: '2em'}}>You agree to accept delivery of the Products at the agreed time and place of delivery, subject to the specific delivery conditions below:</p>
+                    <ul style={{listStyle: 'none', marginBottom: '2em'}}>
+                        <li><b>Delivery Time</b>
+                        <p>The arrival time in our delivery service guarantee will be based on the Vendor's Driver arrival at the gate of your subdivisions, villages, factories, industrial areas and other gated communities (as the case may be); while the arrival time in buildings, hospitals, condominiums and hotels will be based on our Vendor's Driver arrival at the lobby</p></li>
+                        <li><b>Product Availability</b><p>You understand and agree that the Products are subject to availability at the time of delivery. If the Products you ordered are not available, we may offer you a reasonable substitute Product of equivalent quality and price, or we may advise you through phone call (local Philippines), SMS or electronic mail for alternative Products.
+                        </p></li>
+                    </ul>
+                <b>Privacy Policy</b>
+                <p style={{textIndent: '50px', marginBottom: '2em'}}>Our Privacy Policy, and any changes thereto which we may implement from time to time, in connection with how we process your personal information collected from this Site may be found in this link <a href="https://streetfoodexpress.com/" target="_blank">https://streetfoodexpress.com/</a>. You agree that with your acceptance of these Terms and Conditions, you have read, understood and accepted our Privacy Policy.</p>
+
+                <b>Contact Us</b>
+                <p style={{textIndent: '50px', marginBottom: '2em'}}>If you have any questions regarding this Terms and Conditions, please contact us through:<br/><ul>Customer care - <b>(02) 1111-2222</b></ul></p>
+            
+            </div>
         );
     }
     
